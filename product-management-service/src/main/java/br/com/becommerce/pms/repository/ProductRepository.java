747b5a73d78a870ff129bd4163fe08275affde0b
Henrique Sousa <henriqueldcs@gmail.com>
2019-02-26 08:12:37 -0300

    Criação de busca de produtos com paginação (#6)

--- product-management-service/src/main/java/br/com/becommerce/pms/repository/ProductRepository.java
+++ product-management-service/src/main/java/br/com/becommerce/pms/repository/ProductRepository.java
@@ -1,9 +1,17 @@
 package br.com.becommerce.pms.repository;
 
 import br.com.becommerce.pms.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+
+
+    List<Product> findAllByReferenceCodeContaining(String referenceCode, Pageable pageable);
+
+
 }
