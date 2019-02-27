5934aa53ea896c99464106bd430bee014e982a9e
Henrique Sousa <henriqueldcs@gmail.com>
2019-02-27 06:37:21 -0300

    ProductResource movido para o pacote correto (#6)

--- product-management-service/src/main/java/br/com/becommerce/pms/resource/ProductResource.java
+++ product-management-service/src/main/java/br/com/becommerce/pms/resource/ProductResource.java
@@ -1,8 +1,12 @@
-package br.com.becommerce.pms.service;
+package br.com.becommerce.pms.resource;
 
 import br.com.becommerce.pms.model.Product;
+import br.com.becommerce.pms.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
