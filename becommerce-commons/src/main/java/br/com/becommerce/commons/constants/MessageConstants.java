83e7a1e3fe31dfa0a87a6045bd2f738cdd057347
Henrique Sousa <henriqueldcs@gmail.com>
2019-03-01 13:26:10 -0300

    Adição de exclusão de produto à swagger api - (close #9)

--- becommerce-commons/src/main/java/br/com/becommerce/commons/constants/MessageConstants.java
+++ becommerce-commons/src/main/java/br/com/becommerce/commons/constants/MessageConstants.java
@@ -6,9 +6,14 @@ public class MessageConstants {
     public static final String CREATE_SUCCESS_MESSAGE = "Cadastro realizado com sucesso!";
     public static final String UPDATE_SUCCESS_MESSAGE = "Alteração realizada com sucesso!";
     public static final String DELETE_SUCCESS_MESSAGE = "Exclusão realizada com sucesso!";
+    public static final String UNAUTHORIZED_API_KEY = "Api Key inválida!";
+
+
 
     /* EXCEPTION MESSAGES */
     public static final String REFERENCE_CODE_ALREADY_EXISTS = "ReferenceCode já cadastrado!";
     public static final String PRODUCT_NOT_FOUND = "Produto não encontrado!";
+
+
 
 }
